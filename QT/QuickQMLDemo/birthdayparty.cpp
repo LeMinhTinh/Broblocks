4b7d0a1e8d8a2ae0cfe52d26245583c221b79f3b
tml1hc <tinh.leminh@vn.bosch.com>
2019-08-14 16:16:17 +0700

    + update code

---
+++ QT/QuickQMLDemo/birthdayparty.cpp
@@ -0,0 +1,6 @@
+#include "birthdayparty.h"
+
+Birthdayparty::Birthdayparty()
+{
+
+}
---
+++ QT/QuickQMLDemo/birthdayparty.h
@@ -0,0 +1,11 @@
+#ifndef BIRTHDAYPARTY_H
+#define BIRTHDAYPARTY_H
+
+
+class Birthdayparty
+{
+public:
+    Birthdayparty();
+};
+
+#endif // BIRTHDAYPARTY_H
