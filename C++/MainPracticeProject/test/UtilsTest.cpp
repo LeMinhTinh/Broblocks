5a2ac19219a329c7780ed371366aaf70f8bce3e7
Tinh Le <tile@absolute.com>
2023-10-26 10:03:16 +0700

    Update .gitgnore

--- C++/MainPracticeProject/test/UtilsTest.cpp
+++ C++/MainPracticeProject/test/UtilsTest.cpp
@@ -13,5 +13,5 @@ TEST_F(UtilsTest, TestGetMinimumCost)
 {
 	Utils utils;
     int a[10] = { 4, 4, 7, 1, 4 };
-    EXPECT_EQ(utils.getMinimumCost(5, a), 45);
+    EXPECT_EQ(utils.getMinimumCost(5, a), 54);
 }
